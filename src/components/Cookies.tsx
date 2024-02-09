257dad6e593e4287ce66cf0f124a6717e47b5610
Pedro Barreto <pedro.barreto.ne@gmail.com>
2024-02-08 23:28:13 -0300

    attMeiaNoite

--- src/components/Cookies.tsx
+++ src/components/Cookies.tsx
@@ -1,8 +1,25 @@
 "use client";
 
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function CookiesPopUp() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const cookiesAccepted = localStorage.getItem("cookiesAccepted");
+    if (!cookiesAccepted) {
+      setShowPopup(true);
+    }
+  }, []);
+
+  const handleAcceptCookies = () => {
+    localStorage.setItem("cookiesAccepted", "true");
+    setShowPopup(false);
+  };
+
+  if (!showPopup) return null;
+
   return (
     <div className="fixed bottom-0 bg-white w-full flex justify-center z-20">
       <div className="flex text-base justify-center w-[85%] py-4 gap-4">
@@ -10,11 +27,14 @@ export default function CookiesPopUp() {
         <p className="text-base flex items-center">
           🍪 Usamos cookies para garantir que você tenha a melhor experiência em
           nosso site.
-          <Link href="/" className="flex items-center font-bold pl-1">
-            Saiba Mais
+          <Link href="/politica-de-privacidade">
+            <span className="flex items-center font-bold pl-1">Saiba Mais</span>
           </Link>
         </p>
-        <button className="bg-[#254977] rounded px-4 py-2 text-white text-sm font-medium">
+        <button
+          onClick={handleAcceptCookies}
+          className="bg-[#254977] rounded px-4 py-2 text-white text-sm font-medium"
+        >
           Aceitar
         </button>
       </div>
--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -2,13 +2,24 @@
 
 import Image from "next/image";
 import popup from "@/assets/popup.png";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
-
 import close from "@/assets/x-symbol-svgrepo-com.svg";
 
 export default function PopUp() {
+  const initialState = () => {
+    const popUpData = localStorage.getItem("popUpData");
+    if (popUpData) {
+      const { show, expireAt } = JSON.parse(popUpData);
+      const now = new Date().getTime();
+      return now >= expireAt ? true : show;
+    }
+    return true;
+  };
+
+  const [popUp, setPopUp] = useState(initialState);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   const [formData, setFormData] = useState({
     nomeCompleto: "",
     telefone: "",
@@ -21,7 +32,6 @@ export default function PopUp() {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-
     const templateParams = {
       from_name: formData.nomeCompleto,
       telefone: formData.telefone,
@@ -40,59 +50,90 @@ export default function PopUp() {
           nomeCompleto: "",
           telefone: "",
         });
+        const expireAt = new Date().getTime() + 6 * 60 * 60 * 1000;
+        localStorage.setItem(
+          "popUpData",
+          JSON.stringify({ show: false, expireAt })
+        );
+        setShowSuccessMessage(true);
+        setPopUp(false);
+        setTimeout(() => setShowSuccessMessage(false), 2000);
       });
   };
 
-  return (
-    <div className="w-full h-full fixed top-0 popup z-50">
-      <div className="max-w-[500px] my-[1.75rem] mx-auto relative bg-white rounded">
-        <div className="p-3 flex justify-end opacity-70 cursor-pointer">
-          <button>
-            <Image src={close} alt="close" width={20} height={20} />
-          </button>
+  const handleClose = () => {
+    const expireAt = new Date().getTime() + 6 * 60 * 60 * 1000;
+    localStorage.setItem(
+      "popUpData",
+      JSON.stringify({ show: false, expireAt })
+    );
+    setPopUp(false);
+  };
+
+  if (showSuccessMessage) {
+    return (
+      <div className="w-full h-full fixed top-0 left-0 flex justify-center items-center z-50">
+        <div className="bg-white p-4 rounded shadow">
+          <p>Email enviado com sucesso!</p>
         </div>
-        <div className="flex justify-center">
-          <Image src={popup} alt="Imagem popup" width={120} />
-        </div>
-        <div className="flex justify-center items-center gap-2 flex-col mb-4">
-          <h3 className="flex justify-center text-xl font-bold">
-            Olá, tudo bem ?
-          </h3>
-          <p className="flex justify-center text-center w-[90%] opacity-80">
-            Nossa equipe deseja apresentar a Brasil Telhas para você, deixe seu
-            telefone abaixo e entraremos em contato!
-          </p>
-        </div>
-        <form
-          onSubmit={handleSubmit}
-          className="flex items-center flex-col gap-6"
-        >
-          <input
-            type="text"
-            name="nomeCompleto"
-            placeholder="Seu Nome"
-            className="bg-input w-[90%] h-[50px] rounded pl-4"
-            value={formData.nomeCompleto}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="tel"
-            name="telefone"
-            placeholder="Telefone"
-            className="bg-input w-[90%] h-[50px] rounded pl-4"
-            value={formData.telefone}
-            onChange={handleChange}
-            required
-          />
-          <button
-            type="submit"
-            className="w-[90%] bg-button py-2 rounded mb-4 text-white text-base font-semibold"
-          >
-            Enviar
-          </button>
-        </form>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      {popUp && (
+        <div className="w-full h-full fixed top-0 popup z-50">
+          <div className="max-w-[500px] my-[1.75rem] mx-auto relative bg-white rounded">
+            <div className="p-3 flex justify-end opacity-70 cursor-pointer">
+              <button onClick={handleClose}>
+                <Image src={close} alt="close" width={20} height={20} />
+              </button>
+            </div>
+            <div className="flex justify-center">
+              <Image src={popup} alt="Imagem popup" width={120} />
+            </div>
+            <div className="flex justify-center items-center gap-2 flex-col mb-4">
+              <h3 className="flex justify-center text-xl font-bold">
+                Olá, tudo bem ?
+              </h3>
+              <p className="flex justify-center text-center w-[90%] opacity-80">
+                Nossa equipe deseja apresentar a Brasil Telhas para você, deixe
+                seu telefone abaixo e entraremos em contato!
+              </p>
+            </div>
+            <form
+              onSubmit={handleSubmit}
+              className="flex items-center flex-col gap-6"
+            >
+              <input
+                type="text"
+                name="nomeCompleto"
+                placeholder="Seu Nome"
+                className="bg-input w-[90%] h-[50px] rounded pl-4"
+                value={formData.nomeCompleto}
+                onChange={handleChange}
+                required
+              />
+              <input
+                type="tel"
+                name="telefone"
+                placeholder="Telefone"
+                className="bg-input w-[90%] h-[50px] rounded pl-4"
+                value={formData.telefone}
+                onChange={handleChange}
+                required
+              />
+              <button
+                type="submit"
+                className="w-[90%] bg-button py-2 rounded mb-4 text-white text-base font-semibold"
+              >
+                Enviar
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
