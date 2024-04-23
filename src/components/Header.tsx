b4c6d0e5b32cbdc94e99acbd6c749f4c2c40d109
Pedro Barreto <pedro.barreto.ne@gmail.com>
2024-04-23 01:26:46 -0300

    att23/04

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,27 @@ import Logo from "@/assets/logo-princ-white.png";
 import LogoPrincipal from "@/assets/logo-princ.png";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { motion, useCycle } from "framer-motion";
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
 
 const Header = () => {
   const pathname = usePathname();
@@ -38,6 +59,8 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const [isOpen, toggleOpen] = useCycle(false, true);
+
   return (
     <header
       className={`fixed top-0 w-full h-20 flex justify-between items-center z-30 transition duration-300 ${
@@ -62,19 +85,21 @@ const Header = () => {
         </Link>
       </div>
       <div className="w-[50%] pr-[6%]">
-        <div className="md:hidden">
-          <span
+        <div className="md:hidden justify-end flex">
+          <motion.span
             onClick={() => setOpen((ev) => !ev)}
-            className="flex justify-end"
+            className="button-top-ac flex justify-end z-10 relative max-conten"
+            initial={{ rotate: 0 }}
+            animate={{ rotate: open ? 90 : 0 }} // Rotação condicional, por exemplo
+            transition={{ type: "spring", stiffness: 300 }}
           >
             {isAtTop &&
+            !open &&
             pathname != "/marketing" &&
             pathname != "/cookies" &&
             pathname != "/privacidade" ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
                 width="45"
                 height="45"
                 viewBox="0 0 50 50"
@@ -87,8 +112,6 @@ const Header = () => {
             ) : (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
                 width="45"
                 height="45"
                 viewBox="0 0 50 50"
@@ -96,10 +119,16 @@ const Header = () => {
                 <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
               </svg>
             )}
-          </span>
+          </motion.span>
           {open && (
-            <div className="flex flex-col dropDown">
-              <ul className="flex flex-col gap-4">
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.5 }}
+              className="flex flex-col dropDown"
+            >
+              <ul className="flex flex-col gap-4 items-center h-full justify-evenly">
                 <Link className={`${linkStyle}`} href="/sobre">
                   Sobre Nós
                 </Link>
@@ -116,7 +145,7 @@ const Header = () => {
                   Contato
                 </Link>
               </ul>
-            </div>
+            </motion.div>
           )}
         </div>
         <nav
